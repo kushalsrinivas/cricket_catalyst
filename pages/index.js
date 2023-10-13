9ebc12b52f461740bd8ca4b33d347268ec761a52
kushalsrinivas <kushal.s.2005@gmail.com>
2023-10-13 21:12:59 +0530

    added shadcn

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import { Button } from "@/components/ui/button";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-black h-screen w-screen">
       <h1 className="text-4xl font-extrabold text-white w-full text-center flex h-full align-middle justify-center">
         Cricket Catalyst
+
       </h1>
     </main>
   );
