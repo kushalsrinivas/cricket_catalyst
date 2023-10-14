1240e9e610b0e7da7d030ca73e6fda217d84ff8b
kushalsrinivas <kushal.s.2005@gmail.com>
2023-10-14 13:16:26 +0530

    updated src file

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,53 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Button } from "@/components/ui/button";
 const inter = Inter({ subsets: ["latin"] });
-
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useState } from "react";
 export default function Home() {
-  return (
-    <main className="bg-black h-screen w-screen">
-      <h1 className="text-4xl font-extrabold text-white w-full text-center flex h-full align-middle justify-center">
-        Cricket Catalyst
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
 
-      </h1>
-    </main>
+  return (
+    <>
+      <main className="bg-black h-screen w-screen">
+        <h1 className="text-4xl font-extrabold text-white w-[48] text-center flex  align-middle justify-center">
+          crciket
+        </h1>
+        <Card className="w-[350px] ">
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>email and password</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Input
+              type="email"
+              value={email}
+              placeholder="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            ></Input>
+          </CardContent>
+          <CardContent>
+            <Input
+              type="password"
+              value={pwd}
+              placeholder="password"
+              onChange={(e) => {
+                setPwd(e.target.value);
+              }}
+            ></Input>
+          </CardContent>
+        </Card>
+      </main>
+    </>
   );
 }
