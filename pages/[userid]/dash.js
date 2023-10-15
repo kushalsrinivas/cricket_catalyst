5af697fbbb9937649fe1f14593070d4dfb2fec34
kushalsrinivas <kushal.s.2005@gmail.com>
2023-10-15 19:28:12 +0530

    finihsed login

--- pages/[userid]/dash.js
+++ pages/[userid]/dash.js
@@ -1,11 +1,13 @@
 import { useRouter } from "next/router";
 import React from "react";
+import Dashboard from "@/components/Dashboard";
 
-function dash(props) {
+function Dash(props) {
+  const router = useRouter();
   const query = useRouter().query;
   console.log(query);
 
   return <div className="text-black text-3xl font-bold ">{query.userid}</div>;
 }
 
-export default dash;
+export default Dash;
